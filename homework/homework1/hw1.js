c0d7e3fbeb80289a0b45c65023881a3f2f9a182e
Mtuannguyen06 <minhtuannguyennhu@gamil.com>
2023-10-24 19:28:19 +0700

    l2

---
+++ homework/homework1/hw1.js
@@ -0,0 +1,7 @@
+const basket = [["grape", 15], ["apple", 12], ["orange", 10]];
+for(let x = 0; x < basket.length; x++) {
+    for(let y = 1; y >= 0; y--) {
+        console.log(`i have ${basket[x][y]} ${basket[x][y]}`)
+
+    }
+}
