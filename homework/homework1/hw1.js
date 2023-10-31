b5f22c0db4979c75d2c1e8fd70cc5817aaa900e5
Mtuannguyen06 <minhtuannguyennhu@gamil.com>
2023-10-31 19:35:30 +0700

    da

--- homework/homework1/hw1.js
+++
@@ -1,7 +0,0 @@
-const basket = [["grape", 15], ["apple", 12], ["orange", 10]];
-for(let x = 0; x < basket.length; x++) {
-    for(let y = 1; y >= 0; y--) {
-        console.log(`i have ${basket[x][y]} ${basket[x][y]}`)
-
-    }
-}
---
+++ homework/hw3/main.js
@@ -0,0 +1,26 @@
+const rows = (count + 1)/2;
+for (let index = 0; index < rows; index++) {
+   let str = '';
+   for (let index2 = 0; index2 < index * 2 + 1; index2++) {
+      const element = array[index2];
+      
+   }
+   
+}
+
+
+var b = [9, 7, 9, 0, 7, 8, 387, 123, 456];
+
+var c = b.filter(function (item) {
+   return (item % 2 == 0);
+});
+
+console.log(c);
+
+function myFunction() {
+  let person = prompt("Please enter your name", "Harry Potter");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+  }
+}
